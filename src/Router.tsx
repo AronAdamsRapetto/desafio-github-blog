c8753fcccfecb8fa1d9cfb3c3413cffc39ff3d97
Aron Adams <aronrapetto@hotmail.com>
2023-05-02 16:13:00 -0300

    feat: router

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/default'
+import { Feed } from './pages/Feed'
+import { Post } from './pages/Post'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Feed />} />
+        <Route path="/post" element={<Post />} />
+      </Route>
+    </Routes>
+  )
+}
