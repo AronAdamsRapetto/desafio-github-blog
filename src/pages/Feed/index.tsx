59d21c591599954f78bb4778b46d5eedd13a38cc
Aron Adams <aronrapetto@hotmail.com>
2023-05-08 15:46:49 -0300

    refactor: delete PostList component

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,12 +1,74 @@
-import { PostList } from './PostList'
 import {
   FeedPageContainer,
   SearchContainer,
   SearchFormContainer,
+  PostCard,
+  PostsContainer,
 } from './styles'
 import { UserProfile } from './UserProfile'
+import { useCallback, useEffect, useState } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { API } from '../../lib/axios'
+
+interface Post {
+  body: string
+  created_at: string
+  id: number
+  title: string
+}
+
+interface PostResponse extends Post {
+  state: string
+}
 
 export function Feed() {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  const formatPosts = useCallback((postList: PostResponse[]) => {
+    const filteredPosts = postList.filter(
+      (item: PostResponse) => item.state === 'open',
+    )
+
+    const formatedPosts = filteredPosts.map((post: Post) => {
+      const createdAt = new Date(post.created_at)
+      const distanceToNowDate = formatDistanceToNow(createdAt, {
+        locale: ptBR,
+        addSuffix: true,
+      })
+
+      const shortTitle =
+        post.title.length > 38 ? `${post.title.slice(0, 38)}...` : post.title
+
+      return {
+        body: `${post.body.slice(0, 120)}...`,
+        created_at: distanceToNowDate,
+        id: post.id,
+        title: shortTitle,
+      }
+    })
+
+    return formatedPosts
+  }, [])
+
+  const fetchPosts = useCallback(async () => {
+    const {
+      data: { items },
+    } = await API.get('/search/issues', {
+      params: {
+        q: `repo:AronAdamsRapetto/desafio-github-blog`,
+      },
+    })
+
+    const formatedPosts = formatPosts(items)
+
+    setPosts(formatedPosts)
+  }, [formatPosts])
+
+  useEffect(() => {
+    fetchPosts()
+  }, [fetchPosts])
+
   return (
     <FeedPageContainer>
       <UserProfile />
@@ -22,7 +84,18 @@ export function Feed() {
         </SearchFormContainer>
       </SearchContainer>
 
-      <PostList />
+      <PostsContainer>
+        {posts.length &&
+          posts.map((post: Post) => (
+            <PostCard key={post.id}>
+              <div>
+                <h2>{post.title}</h2>
+                <span>{post.created_at}</span>
+              </div>
+              <span>{post.body}</span>
+            </PostCard>
+          ))}
+      </PostsContainer>
     </FeedPageContainer>
   )
 }
