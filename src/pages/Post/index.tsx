b1498b4b8804572b85c79f7f4dce3279a702978d
Aron Adams <aronrapetto@hotmail.com>
2023-05-12 13:28:14 -0300

    feat: fetch post

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faChevronLeft,
@@ -13,8 +13,60 @@ import {
   PostInfoContainer,
   PostPageContainer,
 } from './styles'
+import { useState, useCallback, useEffect } from 'react'
+import { API } from '../../lib/axios'
+import ptBR from 'date-fns/locale/pt-BR'
+import { formatDistanceToNow } from 'date-fns'
+
+interface PostData {
+  postOwner: string
+  title: string
+  body: string
+  comments: number
+  created_at: string
+  html_url: string
+}
+
+interface PostDataResponse extends PostData {
+  user: {
+    login: string
+  }
+}
 
 export function Post() {
+  const [postData, setPostData] = useState<PostData>({} as PostData)
+  const { postId } = useParams()
+
+  const formatPostData = (postData: PostDataResponse) => {
+    const createdAt = new Date(postData.created_at)
+    const distanceToNowDate = formatDistanceToNow(createdAt, {
+      locale: ptBR,
+      addSuffix: true,
+    })
+
+    return {
+      postOwner: postData.user.login,
+      title: postData.title,
+      body: postData.body,
+      comments: postData.comments,
+      created_at: distanceToNowDate,
+      html_url: postData.html_url,
+    }
+  }
+
+  const getPost = useCallback(async () => {
+    const { data } = await API.get(
+      `/repos/AronAdamsRapetto/desafio-github-blog/issues/${postId}`,
+    )
+
+    const formatedData = formatPostData(data)
+    setPostData(formatedData)
+  }, [postId])
+
+  useEffect(() => {
+    getPost()
+  }, [getPost])
+
   return (
     <PostPageContainer>
       <PostHeader>
@@ -28,19 +80,19 @@ export function Post() {
             <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
           </a>
         </NavContainer>
-        <h1>Title</h1>
+        <h1>{postData.title}</h1>
         <PostInfoContainer>
           <div>
             <FontAwesomeIcon icon={faGithub} />
-            <span>usuario</span>
+            <span>{postData.postOwner}</span>
           </div>
           <div>
             <FontAwesomeIcon icon={faCalendarDay} />
-            <span>tempo</span>
+            <span>{postData.created_at}</span>
           </div>
           <div>
             <FontAwesomeIcon icon={faComment} />
-            <span>comentarios</span>
+            <span>{`${postData.comments} comentários`}</span>
           </div>
         </PostInfoContainer>
       </PostHeader>
